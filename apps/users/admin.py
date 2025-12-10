a17e5b6861f00366217f44b7df70a08f6b339d2d
nghiaht2810 <33nghia2002@gmail.com>
2025-12-10 14:12:48 +0700

    up

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,18 @@
+# apps/users/admin.py
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import Profile
 
-# Register your models here.
+# Tạo Inline để hiện Profile ngay trong trang User
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+
+# Hủy đăng ký User cũ và đăng ký lại User mới kèm Profile
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline,)
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
